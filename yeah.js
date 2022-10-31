cfac7dade59539f2c5f53ec75301974a60f5fe66
oraclelens <sophiar10200@gmail.com>
2022-10-31 13:13:32 -0400

    ya

---
+++ yeah.js
@@ -0,0 +1,13 @@
+const avengers = [
+  { heroname: "Thor", first: "Thor", last: "Odinson" },
+  { heroname: "Iron Man", first: "Anthony", last: "Stark" },
+  { heroname: "Captain America", first: "Steve", last: "Rogers" },
+  { heroname: "Black Widow", first: "Natasha", last: "Romanov" },
+  { heroname: "The Hulk", first: "Bruce", last: "Banner" },
+  { heroname: "Hawkeye" },
+  { heroname: "Black Panther" },
+  { heroname: "Dr. Strange", first: "Stephen", last: "Strange" },
+  { heroname: "Scarlet Witch", first: "Wanda", last: "Maximoff" },
+  { heroname: "Vision", first: "Vision", last: "JARVIS" },
+  { heroname: "Spiderman", first: "Peter", last: "Parker" },
+];
