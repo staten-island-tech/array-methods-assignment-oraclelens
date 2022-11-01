e1fd9547953b99fb83fa433825c4db3574a13dc3
oraclelens <sophiar10200@gmail.com>
2022-11-01 12:59:38 -0400

    hi

--- yeah.js
+++ yeah.js
@@ -1,13 +1,20 @@
 const avengers = [
-  { heroname: "Thor", first: "Thor", last: "Odinson" },
-  { heroname: "Iron Man", first: "Anthony", last: "Stark" },
-  { heroname: "Captain America", first: "Steve", last: "Rogers" },
-  { heroname: "Black Widow", first: "Natasha", last: "Romanov" },
-  { heroname: "The Hulk", first: "Bruce", last: "Banner" },
-  { heroname: "Hawkeye" },
-  { heroname: "Black Panther" },
-  { heroname: "Dr. Strange", first: "Stephen", last: "Strange" },
-  { heroname: "Scarlet Witch", first: "Wanda", last: "Maximoff" },
-  { heroname: "Vision", first: "Vision", last: "JARVIS" },
-  { heroname: "Spiderman", first: "Peter", last: "Parker" },
+  { heroname: "Thor", first: "Thor", last: "Odinson", age: 1500 },
+  { heroname: "Iron Man", first: "Anthony", last: "Stark", age: 53 },
+  { heroname: "Captain America", first: "Steve", last: "Rogers", age: 81 },
+  { heroname: "Black Widow", first: "Natasha", last: "Romanov", age: 32 },
+  { heroname: "The Hulk", first: "Bruce", last: "Banner", age: 49 },
+  { heroname: "Hawkeye", first: "Clint", last: "Barton", age: 36 },
+  { heroname: "Black Panther", first: "T'Challa", age: 38 },
+  { heroname: "Dr. Strange", first: "Stephen", last: "Strange", age: 42 },
+  { heroname: "Scarlet Witch", first: "Wanda", last: "Maximoff", age: 28 },
+  { heroname: "Vision", first: "Vision", age: 3 },
+  { heroname: "Spiderman", first: "Peter", last: "Parker", age: 17 },
 ];
+
+avengers
+  .filter((avengers) => avengers.age >= 28)
+
+  .forEach((el) => {
+    console.log(el.first, el.last);
+  });
